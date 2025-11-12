e74a526ed1c4e8d18d45fd027fad098f50a39624
notyusta <xabhista19@gmail.com>
2025-11-12 10:59:33 +0700

    feat: disable auto termination

--- app/Console/Commands/CronJob.php
+++ app/Console/Commands/CronJob.php
@@ -172,24 +172,28 @@ class CronJob extends Command
                 return $number;
             });
 
-            $this->runCronJob('services_terminated', function ($number = 0) {
-                // Terminate orders if due date is overdue for x days
-                Service::where('status', 'suspended')->where('expires_at', '<', now()->subDays((int) config('settings.cronjob_order_terminate', 14)))->each(function ($service) use (&$number) {
-                    TerminateJob::dispatch($service);
+            // NOTE: Termination of services is disabled for now, for safety reasons
+            // $terminateDays = config('settings.cronjob_order_terminate', -1);
+            // if ($terminateDays > 0) {
+            //     $this->runCronJob('services_terminated', function ($number = 0) {
+            //         // Terminate orders if due date is overdue for x days
+            //         Service::where('status', 'suspended')->where('expires_at', '<', now()->subDays((int) config('settings.cronjob_order_terminate', 14)))->each(function ($service) use (&$number) {
+            //             TerminateJob::dispatch($service);
 
-                    $service->update(['status' => 'cancelled']);
-                    // Cancel outstanding invoices
-                    $service->invoices()->where('status', 'pending')->update(['status' => 'cancelled']);
+            //             $service->update(['status' => 'cancelled']);
+            //             // Cancel outstanding invoices
+            //             $service->invoices()->where('status', 'pending')->update(['status' => 'cancelled']);
 
-                    if ($service->product->stock !== null) {
-                        $service->product->increment('stock', $service->quantity);
-                    }
+            //             if ($service->product->stock !== null) {
+            //                 $service->product->increment('stock', $service->quantity);
+            //             }
 
-                    $number++;
-                });
+            //             $number++;
+            //         });
 
-                return $number;
-            });
+            //         return $number;
+            //     });
+            // }
 
             $this->runCronJob('tickets_closed', function ($number = 0) {
                 // Close tickets if no response for x days
