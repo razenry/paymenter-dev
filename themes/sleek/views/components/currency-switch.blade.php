92f186dcdaf6d9d90f2f058b57a6d1bacf911b7c
notyusta <xabhista19@gmail.com>
2025-11-26 12:29:21 +0700

    feat: update currency componentA

--- themes/sleek/views/components/currency-switch.blade.php
+++ themes/sleek/views/components/currency-switch.blade.php
@@ -1,11 +1,4 @@
-<div class="flex flex-col gap-2 min-w-[140px]">
-    <strong class="block text-xs font-semibold uppercase text-base/60 tracking-wide">
-        {{ __('Currency') }}
-    </strong>
-    <x-select
-        wire:model.live="currentCurrency"
-        :options="$this->currencies"
-        placeholder="Select currency"
-        class="min-w-[120px] flex-shrink-0"
-    />
+<div>
+    <strong class="block p-2 text-xs font-semibold uppercase text-base/50"> Currency </strong>
+    <x-select wire:model.live="currentCurrency" :options="$this->currencies" placeholder="Select currency" />
 </div>
