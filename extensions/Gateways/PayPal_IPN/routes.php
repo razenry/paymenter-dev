bb5c7e95fd40949d5633e305d3c5394c8c63ef4a
notyusta <xabhista19@gmail.com>
2025-11-26 13:24:22 +0700

    feat: update ipn

--- extensions/Gateways/PayPal_IPN/routes.php
+++ extensions/Gateways/PayPal_IPN/routes.php
@@ -1,6 +1,7 @@
 <?php
 
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Support\Facades\Route;
 use Paymenter\Extensions\Gateways\PayPal_IPN\PayPal_IPN;
 
-Route::post('/extensions/paypal_ipn/notify', [PayPal_IPN::class, 'notify'])->name('extensions.gateways.paypal_ipn.notify');
+Route::post('/extensions/paypal_ipn/notify', [PayPal_IPN::class, 'notify'])->withoutMiddleware([VerifyCsrfToken::class])->name('extensions.gateways.paypal_ipn.notify');
